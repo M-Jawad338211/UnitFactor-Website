b8a878c484028a36d5e3e43ddb745da2b0ed3e59
M-Jawad338211 <jawad338211@gmail.com>
2025-08-15 17:51:23 +0500

    initial changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,66 @@
-import Image from "next/image";
+import Header from "./header/page";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <Hiring />
+      <Solutions />
+    </div>
+  );
+}
+function Hiring() {
+  return (
+    <div
+      className="w-full h-full bg-cover bg-center"
+      style={{ backgroundImage: "url('/background.png')" }}
+    >
+      <Header />
+      <div>
+        <div className="flex flex-col items-center justify-center h-full mt-20">
+          <div className="flex items-center gap-3">
+            <p className="text-[20px] font-bold text-white">
+              Web Development &amp; UI/UX Design
+            </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            {["SaaS", "Web 3.0", "AI"].map((label) => (
+              <span
+                key={label}
+                className="border border-white text-white px-4 py-1 rounded-full"
+              >
+                {label}
+              </span>
+            ))}
+          </div>
+          <div className="flex flex-col items-center mt-8 mb-10">
+            <p className="text-[40px] font-bold text-white text-center max-w-4xl">
+              Crafting Unique, Purpose-Driven Designs That Capture Attention,
+              Build Trust, and Turn Visitors into Loyal Customers
+            </p>
+            <button className="bg-[#D9D9D9] text-[#0A3253] text-[40px] font-bold px-6 py-3 rounded-2xl mt-10 cursor-pointer">
+              Hire Us
+            </button>
+            <p className="text-[20px] font-bold text-white max-w-xs ml-auto mt-4 ">
+              Subscribe to our services on a monthly basis and get an
+              experienced product designer that will integrate in your team like
+              an in-house professional.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+    </div>
+  );
+}
+
+function Solutions() {
+  return (
+    <div className="flex flex-col p-28 w-full h-full bg-[url('/Rectangle.png')] bg-no-repeat bg-center bg-cover">
+      <p className="text-[64px] font-normal text-white">OUR SOLUTION</p>
+      <p className="text-[24px] font-medium text-white max-w-5xl">
+        We offer a wide range of digital services designed to elevate your
+        business. From custom website development to complete IT solutions, our
+        team delivers innovative, scalable, and results-driven products tailored
+        to your needs.
+      </p>
     </div>
   );
 }
