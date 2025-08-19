67ff3d68a2808708e41d9c7483e339e0059263b8
M-Jawad338211 <jawad338211@gmail.com>
2025-08-19 15:08:09 +0500

    more sections added in home page

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -159,7 +159,7 @@ export default function Header() {
           </button>
           <Link
             href="/contact"
-            className="bg-white text-[#0A3253] font-bold px-4 py-2 rounded-[40px]"
+            className="bg-white hover:bg-[#983232]  text-[#0A3253] hover:text-white font-bold px-4 py-2 rounded-[40px]"
           >
             Contact Us
           </Link>
